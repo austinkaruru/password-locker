bee8d870c2c442cd4d2da79e90fbd0c41ca1fd60
Austin Karuru <karuruaustine@gmail.com>
2018-08-25 13:07:33 +0300

    welcome message

--- cred_test.py
+++ cred_test.py
@@ -3,5 +3,22 @@ from cred import Cred
 
 
 class TestCred(unittest.TestCase):
-    if __name__ == '__main__':
-        unittest.main()
+
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_cred = Cred(
+            "lulu@lu.com", "123")  # create contact object
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_cred.email, "lulu@lu.com")
+        self.assertEqual(self.new_cred.password, "123")
+
+
+if __name__ == '__main__':
+    unittest.main()
